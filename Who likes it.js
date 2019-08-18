dac88eb19b5cfabe9c4ee920fab0bc3b6e6616ea
arstrel <arstrel@gmail.com>
2019-08-18 17:16:12 -0400

    who likes this

---
+++ Who likes it.js
@@ -0,0 +1,14 @@
+const likes = names => {
+let result = 'no one likes this';
+switch (names.length){
+case 0: break;
+case 1: result = `${names[0]} likes this`; break;
+case 2: result = `${names[0]} and ${names[1]} like this`; break;
+case 3: 
+result = `${names[0]}, ${names[1]} and ${names[2]} like this`;
+break;
+default: 
+result = `${names[0]}, ${names[1]} and ${names.length -2} others like this`
+}
+return result
+}
